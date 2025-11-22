8dd4ff6ce4b9b9660608eed01179a88b1b4a8ba5
palmestar <aapmarq@yahoo.com>
2025-11-22 11:33:47 -0800

    Enhance data import script with exploration steps
    Added initial exploration and library loading for NHIS 2021 dataset.

--- scripts/01_data_import.R
+++ scripts/01_data_import.R
@@ -1,10 +1,16 @@
 # Day 1 Task 2
 
+#--- Upload the provided NHIS 2021 dataset
+
 View(NHIS_Data_2021)
+
+#--- load the necessary libraries
 
 library(tidyverse)
 
 library(haven)
+
+#--- Perform an initial exploration: use str(), summary(), and head()
 
 str(NHIS_Data_2021)
 
