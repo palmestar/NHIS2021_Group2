ad838894eedcf874d496b24dc5771c2e78bbab60
isabellav2004-design <isabellav2004@csu.fullerton.edu>
2025-11-23 16:42:05 -0800

    Update 05_multivariate_analysis.R
    Add more comments and labels

--- scripts/05_multivariate_analysis.R
+++ scripts/05_multivariate_analysis.R
@@ -1,16 +1,16 @@
 # Day 4 Task 1 
 
-# Load libraries 
-library(tidyverse)
-install.packages("psych")
+# Load required libraries 
+library(tidyverse) # Data for wrangling and visualization
+install.packages("psych") # Data for matrix correlation
 library(psych)
-library(ggplot2)
+library(ggplot2) # Data for plotting
 nhis_clean <- read_csv("data/nhis_clean.csv")
 
-# Create folder
-dir.create("plots", showWarnings = FALSE)
+# Create output folder
+dir.create("plots", showWarnings = FALSE) # Prevents saving output in an already existing folder
 
-# Scatterplot
+# Scatter plot Height vs Weight
 ggplot(nhis_clean, aes(x = HEIGHTTC_A, y = WEIGHTLBTC_A, color = SEX_A)) +
   geom_point() +
   facet_wrap(~EDUCP_A_recoded) + # You can switch to PHSTAT_A instead
@@ -20,9 +20,10 @@ ggplot(nhis_clean, aes(x = HEIGHTTC_A, y = WEIGHTLBTC_A, color = SEX_A)) +
     y = "Weight"
   )
 
+# Save scatter plot
     ggsave("plots/day4_scatter.png")
 
-# Correlation scatterplot matrix
+# Correlation scatter plot matrix
 pairs.panels(
   nhis_clean %>% select(AGEP_A, WEIGHTLBTC_A, HEIGHTTC_A)
 )
